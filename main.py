bbc199556e5ac4456f64f23274f399c61dfec663
Charlie <cvmcharlie@gmail.com>
2023-10-24 13:13:45 -0400

    Created main and encoder functions with some framework for decoder

---
+++ main.py
@@ -0,0 +1,28 @@
+# Charles Monaghan
+def encode(password):
+	encoded_pass = ''
+	for char in password:
+		encoded_val = int(char) + 3
+		encoded_pass += str(encoded_val)
+	return encoded_pass
+
+if __name__ == "__main__":
+	while True:
+		print('Menu')
+		print('-------------')
+		print('1. Encode')
+		print('2. Decode')
+		print('3. Quit')
+		print()
+		print('Please enter an option: ')
+		choice = input()
+
+		if choice == '1':
+			user_pass = input('Please enter your password to encode:')
+			encrypted_pass = encoder(user_pass)
+		elif choice == '2':
+			pass
+		elif choice == '3':
+			break
+		else:
+			print('Invalid option, please try again')
